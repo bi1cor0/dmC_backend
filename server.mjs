548d0fae5605a4068011a34482b6a45a4c3b2933
yeahbudd01 <billcorcoro@comcast.net>
2024-07-24 11:56:40 -0700

    Created seed route for easy data population of items.

--- server.mjs
+++ server.mjs
@@ -3,6 +3,8 @@ import express from 'express';
 import dotenv from 'dotenv'
 import Characters from './routes/Character_routes.mjs';
 import connectDB from './config/db.mjs';
+//import character from './models/Characters.mjs';
+//import charas from './utilities/character_data.mjs';
 
 //Configurations
 dotenv.config();
@@ -16,6 +18,17 @@ app.use(express.json())
 
 //Routes
 app.use('/characters', Characters)
+
+//Create seed route to fill database with data. Uncomment below and the character and charas vars to utilize seeding function. 
+//app.get('/seed', async (req, res) => {
+//    // This allows us to clear db before putting new data in (optional)
+//    // await Dogs.deleteMany({})
+//  
+//    //This creates all data entries from array in db
+//    await character.create(charas);
+//  
+//    res.send('seeding db');
+//  });
 
 //Listen to express server
 app.listen(PORT, () =>{
