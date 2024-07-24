930d75d88c2c5f26cae0c608b35c504dfed99615
yeahbudd01 <billcorcoro@comcast.net>
2024-07-24 10:49:40 -0700

    Created Character schema and updating with Character routes folder.

---
+++ models/Characters.mjs
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const charSchema = new mongoose.Schema({
+    Player_Name: {
+        type: String,
+        required: true,
+    },
+    Character_Name: {
+        type: String,
+        required: true,
+    },
+    Class: {
+        type: String,
+        required: true,
+    },
+    Race: {
+        type: String,
+        required: true,
+    },
+    Health_Points: {
+        type: Number,
+        required: true
+    },
+    Alive: {
+        type: Boolean,
+        required: true,
+    },
+    Initiative: {
+        type: Number,
+    }
+})
+
+export default mongoose.model('Character', charSchema)
